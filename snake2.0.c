5269620ef7e4d929bd61dfb1e70a28661a8f1505
ewoifuoi <hamimelons@126.com>
2021-03-29 23:38:05 +0800

    2.0正式版

--- snake2.0.c
+++ snake2.0.c
@@ -29,11 +29,19 @@ typedef struct Snake {
     point node[101];
 } snake;
 
+typedef struct Food {
+    point location;
+    int type;
+} food;
 
+food f;
 snake s;
 int mark[30][15];
 char in_put;
 int dir;
+int mode = 0;
+int score;
+int h_score;
 
 void HideCursor();
 void goto_xy(int x, int y);
@@ -46,6 +54,8 @@ void die();
 void make_boundary();
 void init_snake();
 void print(int x, int y, int color, int w);
+void make_food();
+void eat();
 
 int main() {
     hello();
@@ -55,6 +65,12 @@ int main() {
         move();
         if(s.dead == 1) {
             die();
+            if(s.dead == 3) {
+                system("CLS");
+                init();
+                continue;
+            }
+            break;
         }
     }
     return 0;
@@ -96,6 +112,7 @@ void init() {
     dir = 1;
     init_snake();
     memset(mark, 0, sizeof(mark));
+    make_food();
     return ;
 }
 
@@ -115,11 +132,12 @@ void input() {
 void move() {
     print(s.node[s.length].x, s.node[s.length].y, s.color, 0);
     for(int i = s.length; i >= 1; i--) {
+        int xx = s.node[i].x; int yy = s.node[i].y;
         mark[s.node[i].x][s.node[i].y] = 0;
         s.node[i].x = s.node[i - 1].x; s.node[i].y = s.node[i - 1].y;
         mark[s.node[i].x][s.node[i].y] = 1;
+        print(s.node[i].x, s.node[i].y, s.color, 1);
     }
-    print(s.node[0].x, s.node[0].y, s.color, 0);
     switch(dir) {
         case 1 : s.node[0].x++;break;
         case 2 : s.node[0].x--;break;
@@ -127,7 +145,10 @@ void move() {
         case 4 : s.node[0].y++;break;
     }
     print(s.node[0].x, s.node[0].y, s.color, 1);
-    if(s.node[0].x == 0 && s.node[0].y == 0 && s.node[0].x == 30 && s.node[0].y == 15) {
+    if(s.node[0].x == f.location.x && s.node[0].y == f.location.y) {
+        eat();
+    }
+    if(s.node[0].x == 0 || s.node[0].y == 0 || s.node[0].x == 29 || s.node[0].y == 14) {
         s.dead = 1;
         return ;
     }
@@ -138,7 +159,34 @@ void move() {
     Sleep(s.speed);
     return ;
 }
+
 void die() {
+    FILE *fp;
+    if((fp = fopen("snake_h_score.txt", "r+")) == NULL) {
+        fp = fopen("snake_h_score.txt", "w+");
+    }
+    fscanf(fp,"%d", &h_score);
+    s.dead = 1;
+    if(score > h_score) {
+        fclose(fp);
+        fp = fopen("snake_h_score.txt", "w+");
+        fprintf(fp,"%d",score);
+        h_score = score;
+
+    }
+    system("CLS");
+    printf("\n\n\n\n\n\n\n\t\t\t\t\t    你死了...\n\n");
+    printf("\n\n\t\t\t\t\t当前分数为 : %d\n\n",score);
+    printf("\t\t\t\t\t历史最高分数为 : %d\n\n",h_score);
+    printf("\n\t\t\t\t  按 r键重来 , 按其余键结束游戏\n\n\n\n\n\n\n\n\n");
+    char c = getch();
+    if(c == 'r') {
+        s.dead = 3;
+    }
+    else {
+        s.dead = 2;
+    }
+    fclose(fp);
     return ;
 }
 
@@ -182,29 +230,29 @@ void make_boundary() {
 void init_snake() {
     s.length = 5;
     s.dead = 0;
-    s.speed = 70;
+    s.speed = 100;
     s.color = 7;
     s.node[0].x = 6;s.node[0].y = 6;
     mark[s.node[0].x][s.node[0].y] = 1;
     goto_xy(6,6);
     printf("O");
-    s.node[1].x = 6;s.node[1].y = 5;
+    s.node[1].x = 5;s.node[1].y = 6;
     mark[s.node[1].x][s.node[1].y] = 1;
     goto_xy(5,6);
     printf("O");
-    s.node[2].x = 6;s.node[2].y = 4;
+    s.node[2].x = 4;s.node[2].y = 6;
     mark[s.node[2].x][s.node[2].y] = 1;
     goto_xy(4,6);
     printf("O");
-    s.node[3].x = 6;s.node[3].y = 3;
+    s.node[3].x = 3;s.node[3].y =6;
     mark[s.node[3].x][s.node[3].y] = 1;
     goto_xy(3,6);
     printf("O");
-    s.node[4].x = 6;s.node[4].y = 2;
+    s.node[4].x = 2;s.node[4].y = 6;
     mark[s.node[4].x][s.node[4].y] = 1;
     goto_xy(2,6);
     printf("O");
-    s.node[5].x = 6;s.node[5].y = 1;
+    s.node[5].x = 1;s.node[5].y = 6;
     mark[s.node[5].x][s.node[5].y] = 1;
     goto_xy(1,6);
     printf("O");
@@ -228,7 +276,35 @@ void print(int x, int y, int color, int w) {
             printf("X");
             break;
         }
+        case 3 : {
+            printf("♥");
+            break;
+        }
     }
     set_console_color(7);
+    return ;
+}
+void make_food() {
+    srand(time(0));
+    f.location.x = rand() % 28 + 1;
+    f.location.y = rand() % 13 + 1;
+    f.type = rand() % 4 ;
+    int COLOR[4]={7 ,5, 2, 11};
+    print(f.location.x, f.location.y, COLOR[f.type], 3);
+    return ;
+}
+
+void eat() {
+    int TYPE_SCORE[4] = {10, 50, 10, 10};
+    print(f.location.x, f.location.y, 7, 0);
+    mode = f.type;
+    score += TYPE_SCORE[f.type];
+    if(f.type == 1) {
+        s.length *= 2;
+    }
+    else {
+        s.length++;
+    }
+    make_food();
     return ;
 }
